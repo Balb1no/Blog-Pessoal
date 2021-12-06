0f2b76c94ee33bc0d265e98b58c2ca3b4ac4eacd
Gabriel Balbino <gbalbino098@gmail.com>
2021-12-06 16:17:18 -0300

    Model

--- src/main/java/com/blogpessoal/model/Usuario.java
+++ src/main/java/com/blogpessoal/model/Usuario.java
@@ -37,11 +37,14 @@ public class Usuario {
 	@Size(min = 5, message = "O atributo senha deve ter no mínimo 5 caracteres")
 	private String senha;
 	
+	private String foto;
+	
+	private String tipo;
+	
 	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List <Postagem> postagem;
-
-	// Primeiro método Construtor
+	
 
 	public Usuario(long id, String nome, String usuario, String senha) {
 		this.id = id;
@@ -50,7 +53,6 @@ public class Usuario {
 		this.senha = senha;
 	}
 
-	// Segundo método Construtor
 
 	public Usuario() {	}
 
@@ -86,6 +88,28 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
+	
+
+	public String getFoto() {
+		return foto;
+	}
+
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
 
 	public List<Postagem> getPostagem() {
 		return this.postagem;
